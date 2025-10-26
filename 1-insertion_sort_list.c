252722ae67c0701a0289c95ea06af5ea2cc254b4
lina_aziz <lenaexo0@gmail.com>
2025-10-26 18:02:51 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,55 @@
 #include "sort.h"
+
 /**
  * insertion_sort_list - sorts a doubly linked list
- * @list:...
- * Return:...
+ * @list: pointer to pointer to the head of the list
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-
-	listint_s *sorted = NULL;
-	listint_s *curr = *list;
+	struct listint_s *sorted = NULL;
+	struct listint_s *curr;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
+	curr = *list;
+
 	while (curr != NULL)
 	{
-		listint_t *next = curr->next;
+		struct listint_s *next = curr->next;
 
 		if (sorted == NULL || sorted->n >= curr->n)
 		{
 			curr->next = sorted;
+
 			if (sorted != NULL)
 				sorted->prev = curr;
+
 			sorted = curr;
 			sorted->prev = NULL;
 		}
 		else
 		{
-			listint_t *current_sorted = sorted;
+			struct listint_s *current_sorted = sorted;
 
 			while (current_sorted->next != NULL &&
-current_sorted->next->n < curr->n)
+			       current_sorted->next->n < curr->n)
 			{
 				current_sorted = current_sorted->next;
 			}
+
 			curr->next = current_sorted->next;
+
 			if (current_sorted->next != NULL)
 				current_sorted->next->prev = curr;
+
 			current_sorted->next = curr;
 			curr->prev = current_sorted;
 		}
+
 		curr = next;
 	}
+
 	*list = sorted;
-	return;
 }
