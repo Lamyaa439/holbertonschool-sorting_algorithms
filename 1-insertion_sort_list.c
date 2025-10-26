e83ae1d9dcf41a2a7398d3adc02b518426ef794d
lina_aziz <lenaexo0@gmail.com>
2025-10-26 17:17:04 +0300

    Create 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list:...
+ * Return:...
+ */
+void insertion_sort_list(listint_t **list);
+if (*list == NULL)  
+return (*list);
+  struct listint_s sorted = NULL;
+struct listint_s curr = *list;
+  while (curr != NULL) {
+      
+    struct listint_s *next = curr->next;
+
+        if (sorted == NULL || sorted->data >= curr->data) {
+            curr->next = sorted;
+
+            if (sorted != NULL) sorted->prev = curr;
+
+            sorted = curr;
+            sorted->prev = NULL;
+
+        } 
+        else {
+          
+            struct listint_s *current_sorted = sorted;
+
+            while (current_sorted->next != NULL &&
+                   current_sorted->next->data < curr->data) {
+                current_sorted = current_sorted->next;
+            }
+
+            curr->next = current_sorted->next;
+
+            if (current_sorted->next != NULL)
+                current_sorted->next->prev = curr;
+
+            current_sorted->next = curr;
+            curr->prev = current_sorted;
+        }
+
+        curr = next;
+    }
+
+    return sorted;
+}
