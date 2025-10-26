f8e88c335831f3fccbbd2059aee0a28899660860
lina_aziz <lenaexo0@gmail.com>
2025-10-26 18:13:12 +0300

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: list with numbers
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		tmp = i;
+		flag = 0;
+
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag = 1;
+			}
+		}
+
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+
+		if (flag == 1)
+			print_array(array, size);
+	}
+}
