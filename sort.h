6a0a3febb8b6890accfd6c1815e8ae3a7abc4d13
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-26 13:11:26 +0000

    create README.md and other files

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+#endif
